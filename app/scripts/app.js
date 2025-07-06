943c43011aa9e8b75edb10d4aec09d74f328e488
Alexander Prypyshnyi <alexpryp@gmail.com>
2025-07-06 12:20:36 +0300

    ADD | burger menu

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+const burgerMenu = document.querySelector('.mnu-button__menu');
+
+if (burgerMenu) {
+  const menuBackground = document.querySelector('.main-menu-background');
+  const mainMenu = document.querySelector('.main-menu');
+
+  burgerMenu.addEventListener('click', menu => {
+    burgerMenu.classList.toggle('active');
+
+    if (burgerMenu.classList.contains('active')) {
+      menuBackground.classList.add('active');
+      mainMenu.classList.add('active');
+    } else {
+      menuBackground.classList.remove('active');
+      mainMenu.classList.remove('active');
+    }
+  });
+}
